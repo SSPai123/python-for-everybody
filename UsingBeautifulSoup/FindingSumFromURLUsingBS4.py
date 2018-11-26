ec38b93c404760146295d66c0a77670a6a853d7d
Sukanya Pai <sukanyasurendrapai@gmail.com>
2018-11-27 00:42:28 +0530

    Create

---
+++ UsingBeautifulSoup/FindingSumFromURLUsingBS4.py
@@ -0,0 +1,28 @@
+# To run this, you can install BeautifulSoup
+# https://pypi.python.org/pypi/beautifulsoup4
+
+# Or download the file
+# http://www.py4e.com/code3/bs4.zip
+# and unzip it in the same directory as this file
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter - ')
+html = urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html, "html.parser")
+
+# Retrieve all of the span tags
+tags = soup('span')
+sumTot=0
+for tag in tags:
+    #Get the content from the span tag
+    sumTot+=int(tag.contents[0])
+print("Count ", len(tags))
+print("Sum ", sumTot)
